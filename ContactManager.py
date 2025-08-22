4858c65f9ef94fd45c738dcd3a84cb06105df6ea
Ch Lokesh <lokeshchirumamilla2104@gmail.com>
2025-08-22 14:44:12 +0530

    boiler_plate

---
+++ ContactManager.py
@@ -0,0 +1,27 @@
+from contact import Contact
+class ContactManager:
+    def __init__(self,filename='contacts.txt'):
+        self.filename=filename
+        self.contacts = self.load_contacts()
+    def load_contacts(self):
+        with open(self.filename,'r+') as f:
+            return f.readlines()
+    def save_contacts(self):
+        with open(self.filename,'w+') as f:
+            f.writelines(self.contacts)
+    def addContact(self,contact):
+        pass
+
+    def deleteContact(self,name):
+        pass
+
+    def searchContact(self,name):
+        pass
+
+    def displayContact(self):
+        pass
+
+    def updateContact(self):
+        pass
+
+        
